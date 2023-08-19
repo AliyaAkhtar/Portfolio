b178b7efb2e98da532ae5475fc37f4b603ebd40e
AliyaAkhtar <aliya10akhtar3a@gmail.com>
2023-08-19 22:01:47 +0500

    loader added

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+document.getElementById("submit-btn").addEventListener("click", function () {
+    // Get the form data
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
 
-function sendEmail(){
-    var params = {
-        from_name : document.getElementById("fullName").value,
-        email_id : document.getElementById("email_id").value,
-        message : document.getElementById("message").value
-    }
-    emailjs.send("service_iy75nog", "template_d521mib", params).then(function (res) {
-        alert("Success!" + res.status);
-    })
-}
+    // Display the loader
+    document.getElementById("submit-btn").classList.add("loading");
+    document.getElementById("loader").classList.remove("hidden");
+
+    // Use EmailJS to send the email
+    emailjs.send("service_iy75nog", "template_d521mib", {
+        to_name: name,
+        from_name: email,
+        message: message
+    }).then(function () {
+
+        // Hide the loader after sending email
+        document.getElementById("submit-btn").classList.remove("loading");
+        document.getElementById("loader").classList.add("hidden");
+        // Show alert message
+        alert("Email sent successfully!");
+        // Reset the form
+        document.getElementById("contact-form").reset();
+    }).catch(function (error) {
+        // Handle error here (e.g., show an error message)
+        console.error("Error sending email:", error);
+
+        // Hide the loader on error
+        document.getElementById("submit-btn").classList.remove("loading");
+        document.getElementById("loader").classList.add("hidden");
+    });
+});
+});
